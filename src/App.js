3798d469f5d15e5a250f1cf5aa98441c8dd25849
Sienna <remicknsisi@gmail.com>
2023-08-04 10:28:16 -0400

    added git config --global core.ignorecase false

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Blog from "./components/Blog";
 import Contact from "./components/Contact";
 
 export default function App() {
-  console.log('hi from app')
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <About />
